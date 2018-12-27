7037bdf63502d8d3bb99d915c9f868d2405278e8
luxn <maximilian.herbers@outlook.com>
2018-12-27 19:32:17 +0100

    added diesel and updated to rust 2018

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+extern crate snowflake;
 extern crate actix_web;
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
 
 use actix_web::{server, App, HttpRequest, Responder};
 
 fn greet(req: &HttpRequest) -> impl Responder {
     let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+
+    format!("Hello {}!\n{:?}", to, users)
 }
 
 fn main() {
@@ -12,7 +17,7 @@ fn main() {
         App::new()
             .resource("/", |r| r.f(greet))
     })
-        .bind("localhost:8080")
+        .bind("localhost:8081")
         .expect("Can not bind to port 8080 on localhost")
-        .run();    
+        .run();
 }
